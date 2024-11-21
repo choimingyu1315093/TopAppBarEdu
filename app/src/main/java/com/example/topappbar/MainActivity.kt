b836f84eeac7a20afa180194a1093d4ca744a1fe
choimingyu <alsrb1315093@gmail.com>
2024-11-21 15:49:42 +0900

    작업 완료

--- app/src/main/java/com/example/topappbar/MainActivity.kt
+++ app/src/main/java/com/example/topappbar/MainActivity.kt
@@ -4,29 +4,51 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.topappbar.ui.theme.TopAppBarTheme
 
-//안녕하세요 코딩일기 채널의 최대리입니다.
-//오늘은 TopAppBar Composable 배워보겠습니다.
-//TopAppBar는 앱의 상단에 표시되는 앱바에요
-//주로 앱의 제목이나 액션 버튼을 활용해서 페이지를 전환하거나 할 때 사용됩니다.
-//그럼 코드보면서 설명드리도록 하겠습니다.
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TopAppBarTheme {
+                val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
                 Scaffold(
-                    modifier = Modifier.fillMaxSize()
+                    topBar = {
+                        BasicTopAppBar()
+                    },
+                    modifier = Modifier.fillMaxSize().nestedScroll(scrollBehavior.nestedScrollConnection)
+
                 ) { innerPadding ->
                     Column (
                         modifier = Modifier
@@ -40,9 +62,48 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BasicTopAppBar(modifier: Modifier = Modifier){
-    TopAppBar(
+    val isDetail = remember { mutableStateOf(false) }
 
+    TopAppBar(
+        title = {
+            Text(
+                text = "BasicTopAppBar"
+            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.Cyan
+        ),
+        actions = {
+            IconButton(
+                onClick = {
+                    isDetail.value = !isDetail.value
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Search Icon"
+                )
+            }
+        },
+        navigationIcon = {
+            if(isDetail.value){
+                IconButton(
+                    onClick = {
+                        isDetail.value = !isDetail.value
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Back Icon"
+                    )
+                }
+            }else {
+                Box{}
+            }
+        },
+        scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     )
 }
